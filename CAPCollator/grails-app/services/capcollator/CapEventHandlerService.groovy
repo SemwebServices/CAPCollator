157c3b7825f206f983f6aa0fab0ee48ce67e696d
Ian Ibbotson <ianibbo@gmail.com>
2017-10-31 07:58:21 +0000

    tidy

--- CAPCollator/grails-app/services/capcollator/CapEventHandlerService.groovy
+++ CAPCollator/grails-app/services/capcollator/CapEventHandlerService.groovy
@@ -44,31 +44,32 @@ class CapEventHandlerService {
                 // We enrich the parsed JSON document with a version of the polygon that ES can index to make the whole
                 // database of alerts geo searchable
                 area.cc_poly = [ type:'polygon', coordinates:[ inner_polygon_ring ] ]
+              }
 
-                // If we got a polygon AND there was an info.area.geocode then we can look to see if we should cache that code
-                // this is duff -- an alert can have many polygons and many geocodes, so the assumption here is wrong. 
-                if ( 1==2 ) {
-                  if ( area.geocode && area.geocode.value && area.geocode.valueName ) {
-                    log.debug("CAP Alert contains polygon and geocode - cache value - ${area.geocode}");
-                    def authorities = area.geocode.valueName instanceof List ? area.geocode.valueName : [ area.geocode.valueName ]
-                    def symbols = area.geocode.value instanceof List ? area.geocode.value : [ area.geocode.value ]
+              // If we got a polygon AND there was an info.area.geocode then we can look to see if we should cache that code
+              // this is duff -- an alert can have many polygons and many geocodes, so the assumption here is wrong. 
+              if ( 1==2 ) {
+                if ( area.geocode && area.geocode.value && area.geocode.valueName ) {
+                  log.debug("CAP Alert contains polygon and geocode - cache value - ${area.geocode}");
+                  def authorities = area.geocode.valueName instanceof List ? area.geocode.valueName : [ area.geocode.valueName ]
+                  def symbols = area.geocode.value instanceof List ? area.geocode.value : [ area.geocode.value ]
   
-                    Iterator i1=authorities.iterator()
-                    Iterator i2=symbols.iterator()
-                    for (; i1.hasNext() && i2.hasNext(); ) {
-                      
-                      try {
-                        gazService.cache(i1.next(), i2.next(), inner_polygon_ring);
-                      }
-                      catch ( Exception e ) {
-                        log.error("problem trying to cache gaz entry",e);
-                      }
+                  Iterator i1=authorities.iterator()
+                  Iterator i2=symbols.iterator()
+                  for (; i1.hasNext() && i2.hasNext(); ) {
+                    
+                    try {
+                      gazService.cache(i1.next(), i2.next(), inner_polygon_ring);
+                    }
+                    catch ( Exception e ) {
+                      log.error("problem trying to cache gaz entry",e);
                     }
                   }
                 }
               }
-              else if ( area.geocode ) {
-                log.debug("CAP Alert has no polygon, but does have geocode : ${area.geocode} ");
+
+              area.geocode.each { gc ->
+                log.debug("CAP Alert has geocode : ${geocode} ");
               }
             }
           }
