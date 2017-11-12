ad326988db332b8e203f3ce1a7ea40ed2c445bf3
Ian Ibbotson <ianibbo@gmail.com>
2017-11-12 11:00:05 +0000

    updated

--- CAPCollator/grails-app/services/capcollator/RssEventHandlerService.groovy
+++ CAPCollator/grails-app/services/capcollator/RssEventHandlerService.groovy
@@ -38,93 +38,86 @@ class RssEventHandlerService {
       log.debug("RssEventHandlerService::handleNotification processing ${list_of_links.size()} elements");
 
       list_of_links.each { link ->
+
         log.debug("Looking in link attribute for cap ${link}");
 
         // Different feeds behave differently wrt properly setting the type attribute. 
         // Until we get to grips a little better - try and parse every link - and if we manage to parse XML, see if the root node is a cap element
 
-          log.debug("  -> processing link ${link})");
+        log.debug("  -> processing link ${link})");
 
-          try {
-            def ts_2 = System.currentTimeMillis();
-            def cap_link = link.'@href'
+        try {
+          def ts_2 = System.currentTimeMillis();
+          def cap_link = link.'@href'
 
-            log.debug("test ${cap_link}");
-            java.net.URL cap_link_url = new java.net.URL(cap_link)
-            java.net.URLConnection conn = cap_link_url.openConnection()
-            conn.setConnectTimeout(5000);
-            conn.setReadTimeout(5000);
-            // conn.setAllowUserInteraction(false);         
-            // conn.setDoOutput(true);
+          log.debug("test ${cap_link}");
+          java.net.URL cap_link_url = new java.net.URL(cap_link)
+          java.net.URLConnection conn = cap_link_url.openConnection()
+          conn.setConnectTimeout(5000);
+          conn.setReadTimeout(5000);
             
-            def detected_content_type = conn.getContentType()
-            log.debug("URL Connection reports content type ${detected_content_type}");
+          def detected_content_type = conn.getContentType()
+          log.debug("URL Connection reports content type ${detected_content_type}");
 
-            if ( detected_content_type &&
-                 ( detected_content_type.toLowerCase().startsWith('text/xml') ||
-                   detected_content_type.toLowerCase().startsWith('application/octet-stream') ||   // Because of http://www.gestiondelriesgo.gov.co
-                   detected_content_type.toLowerCase().startsWith('application/xml') ) ) {
-              def parser = new XmlSlurper()
-              parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false) 
-              parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-              // def parsed_cap = parser.parse(cap_link)
-              def parsed_cap = parser.parse(conn.getInputStream())
+          if ( detected_content_type &&
+               ( detected_content_type.toLowerCase().startsWith('text/xml') ||
+                 detected_content_type.toLowerCase().startsWith('application/octet-stream') ||   // Because of http://www.gestiondelriesgo.gov.co
+                 detected_content_type.toLowerCase().startsWith('application/xml') ) ) {
+
+            def parser = new XmlSlurper()
+            parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false) 
+            parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            def parsed_cap = parser.parse(conn.getInputStream())
   
-              //                      .declareNamespace(
-              //                                        xmlschema:"http://www.w3.org/2001/XMLSchema",
-              //                                        cap11:"urn:oasis:names:tc:emergency:cap:1.1",
-              //                                        cap12:"urn:oasis:names:tc:emergency:cap:1.2")
-          
-              if ( parsed_cap.identifier ) {
-                num_cap_files_found++
-                def ts_3 = System.currentTimeMillis();
-                log.debug("Managed to parse link, looks like CAP :: handleNotification ::\"${parsed_cap.identifier}\"");
+            if ( parsed_cap.identifier ) {
+              num_cap_files_found++
+              def ts_3 = System.currentTimeMillis();
+              log.debug("Managed to parse link, looks like CAP :: handleNotification ::\"${parsed_cap.identifier}\"");
   
-                def entry = domNodeToString(parsed_cap)
+              def entry = domNodeToString(parsed_cap)
 
-                def latest_expiry = null;
-                def latest_effective = null;
-                parsed_cap.info.each { info_element ->
-                  latest_expiry = info_element.expires?.text()
-                  latest_effective = info_element.effective?.text()
-                }
-  
-                log.debug("latest_expiry is ${latest_expiry}");
-                def alert_metadata = [:]
-                alert_metadata.CCHistory=[]
-                alert_metadata.CCHistory.add(["event":"CAPCollator notified","timestamp":ts_1]);
-                alert_metadata.CCHistory.add(["event":"CAPCollator fetch alert","timestamp":ts_2]);
-                alert_metadata.CCHistory.add(["event":"CAPCollator publish CAP event","timestamp":ts_3]);
-                alert_metadata.SourceUrl = cap_link
-
-                if ( latest_expiry && latest_expiry.trim().length() > 0 )
-                  alert_metadata.Expires = latest_expiry
-
-                if ( latest_effective && latest_effective.trim().length() > 0 )
-                  alert_metadata.Effective = latest_effective
-
-                // Render the cap object as JSON - We wrap the converted message in an object so we can add some metadata about
-                // processing time - for stats / tracking the delay through the system
-                String json_text = '{ "AlertMetadata":'+toJson(alert_metadata)+',"AlertBody":'+capcollator.Utils.XmlToJson(entry)+'}'
-  
-                // http://www.nws.noaa.gov/geodata/ tells us how to understand geocode elements
-                // Look at the various info.area elements - if the "polygon" element is null see if we can find an info.area.geocode we understand well enough to expand
-                broadcastCapEvent(json_text, context.properties.headers)
+              def latest_expiry = null;
+              def latest_effective = null;
+              parsed_cap.info.each { info_element ->
+                latest_expiry = info_element.expires?.text()
+                latest_effective = info_element.effective?.text()
               }
-              else {
-                log.warn("No valid CAP from ${cap_link} -- consider improving rules for handling this");
-              }
+  
+              log.debug("latest_expiry is ${latest_expiry}");
+              def alert_metadata = [:]
+              alert_metadata.CCHistory=[]
+              alert_metadata.CCHistory.add(["event":"CAPCollator notified","timestamp":ts_1]);
+              alert_metadata.CCHistory.add(["event":"CAPCollator fetch alert","timestamp":ts_2]);
+              alert_metadata.CCHistory.add(["event":"CAPCollator publish CAP event","timestamp":ts_3]);
+              alert_metadata.SourceUrl = cap_link
+
+              if ( latest_expiry && latest_expiry.trim().length() > 0 )
+                alert_metadata.Expires = latest_expiry
+
+              if ( latest_effective && latest_effective.trim().length() > 0 )
+                alert_metadata.Effective = latest_effective
+
+              // Render the cap object as JSON - We wrap the converted message in an object so we can add some metadata about
+              // processing time - for stats / tracking the delay through the system
+              String json_text = '{ "AlertMetadata":'+toJson(alert_metadata)+',"AlertBody":'+capcollator.Utils.XmlToJson(entry)+'}'
+  
+              // http://www.nws.noaa.gov/geodata/ tells us how to understand geocode elements
+              // Look at the various info.area elements - if the "polygon" element is null see if we can find an info.area.geocode we understand well enough to expand
+              broadcastCapEvent(json_text, context.properties.headers)
             }
             else {
-              log.warn("${cap_link} (content type ${detected_content_type}) seems not to be XML and therefore cannot be a CAP message - skipping");
+              log.warn("No valid CAP from ${cap_link} -- consider improving rules for handling this");
             }
           }
-          catch ( Exception e ) {
-            log.error("problem handling cap alert ${body} ${context} ${e.message}");
+          else {
+            log.warn("${cap_link} (content type ${detected_content_type}) seems not to be XML and therefore cannot be a CAP message - skipping");
           }
-          finally {
-            log.debug("ATOM Checker Task Complete");
-          }
+        }
+        catch ( Exception e ) {
+          log.error("problem handling cap alert ${body} ${context} ${e.message}");
+        }
+        finally {
+          log.debug("ATOM Checker Task Complete");
         }
       }
     }
