585b564d66fedee1da1445be559a393c869c3afe
Ian Ibbotson <ianibbo@gmail.com>
2018-08-26 15:22:02 +0100

    Commenting and prep for static feed writer

--- CAPCollator/grails-app/rabbit-consumers/capcollator/CAPSubscriptionMatchConsumer.groovy
+++ CAPCollator/grails-app/rabbit-consumers/capcollator/CAPSubscriptionMatchConsumer.groovy
@@ -13,5 +13,8 @@ class CAPSubscriptionMatchConsumer {
 
   def handleMessage(def body, MessageContext context) {
     log.debug("CAPSubscriptionMatchConsumer::handleMessage(${body},${context})");
+    // If we have a static feeds DIR configured, log alerts in that file.
+    // if ( grailsApplication.config.staticFeedsDir != null ) {
+    // }
   }
 }
