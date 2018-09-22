6aa702cc587371ce038174d8e734dceb8670c400
Ian Ibbotson <ianibbo@gmail.com>
2018-09-22 12:12:31 +0100

    updated static site generation

--- CAPCollator/grails-app/services/capcollator/StaticFeedService.groovy
+++ CAPCollator/grails-app/services/capcollator/StaticFeedService.groovy
@@ -73,6 +73,10 @@ class StaticFeedService {
 
     def fileWriter = new FileWriter(path+'/rss.xml');
     def rssBuilder = new MarkupBuilder(fileWriter)
+
+    def sdf = new SimpleDateFormat('yyyy-MM-dd\'T\'HH:mm:ssX')
+    def pub_date_str = sdf.format(new Date());
+
     rssBuilder.'rss'(// 'xmlns':'http://www.w3.org/2005/Atom', -- The namespace for the document is not ATON
                      'xmlns:rss':'http://www.rssboard.org/rss-specification',
                      'xmlns:atom':'http://www.w3.org/2005/Atom',
@@ -82,17 +86,17 @@ class StaticFeedService {
         'link'(rel:'self',href:"${grailsApplication.config.staticFeedsBaseUrl}/${subname}/rss.xml", type:"application/rss+xml")
         'link'(rel:'alternate',title:'RSS',href:"${grailsApplication.config.staticFeedsBaseUrl}/${subname}/rss.xml", type:"application/rss+xml")
         title("Latest Valid CAP alerts received, ${subname}")
-        link("https://s3-eu-west-1.amazonaws.com/alert-feeds/${subname}")
+        link("${grailsApplication.config.staticFeedsBaseUrl}/${subname}")
         description("This feed lists the most recent valid CAP alerts uploaded to the Filtered Alert Hub.")
         language("en");
         copyright("public domain");
-        // pubDate("Sun, 26 Aug 2018 21:23:20 GMT");
-        // lastBuildDate("Wed, 4 Jan 2017 11:31:34 GMT");
+        pubDate(pub_date_str);
+        lastBuildDate(pub_date_str);
         docs("http://blogs.law.harvard.edu/tech/rss");
         image {
           title("Latest Valid CAP alerts received, ${subname}");
-          url("https://s3-eu-west-1.amazonaws.com/alert-feeds/${subname}/capLogo.jpg");
-          link("https://s3-eu-west-1.amazonaws.com/alert-feeds/${subname}/rss.xml");
+          url("${grailsApplication.config.staticFeedsBaseUrl}/${subname}/capLogo.jpg");
+          link("${grailsApplication.config.staticFeedsBaseUrl}/${subname}/rss.xml");
         }
       }
     }
@@ -115,35 +119,23 @@ class StaticFeedService {
   
       //Edit File e.g. append an element called foo with attribute bar
   
-      // <item>
-      //   <title>Official weather warning: warning of wind gusts</title>
-      //   <link>https://alert-hub.s3.amazonaws.com/de-dwd-en/2018/08/26/15/03/2018-08-26-15-03-23-418.xml</link>
-      //   <description>There is a risk of wind gusts (level 1 of 4).
-      //                    Max. gusts: &lt; 60 km/h; Wind direction: south then south-west</description>
-      //   <category>Met</category>
-      //   <pubDate>Sun, 26 Aug 2018 14:51:00 </pubDate>
-      //   <guid isPermaLink="false">2.49.0.1.276.0.DWD.PVW.1535295060000.a89248a8-6521-4fda-b3e7-1a28a05e8f13.ENG</guid>
-      //   <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/">CAP@dwd.de (DWD / Nationales Warnzentrum Offenbach)</dc:creator>
-      //   <dc:date xmlns:dc="http://purl.org/dc/elements/1.1/">2018-08-26T14:51:00</dc:date>
-      // </item>
-  
-      // node.AlertBody.identifier
-      // node.AlertBody.sender
-      // node.AlertBody.sent
-      // node.AlertBody.status
-      // node.AlertBody.info [
-      //              language, category, description, senderName,....
-      // ]
-
       log.debug("Get first info section");
       def info = getFirstInfoSection(node);
 
+      def formatted_pub_date = null;
+      try {
+        def sdf = new SimpleDateFormat('yyyy-MM-dd\'T\'HH:mm:ssX')
+        def alert_date = sdf.parse(node?.AlertBody?.sent);
+        formatted_pub_date = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z").format(alert_date);
+      }
+      catch ( Exception e ) {
+      }
+
       def new_item_node = xml.channel[0].appendNode( 'item' );
       new_item_node.appendNode( 'title', info?.headline ?: info?.description );
-      new_item_node.appendNode( 'originalLink', node?.AlertMetadata?.SourceUrl);
-      new_item_node.appendNode( 'link', "${grailsApplication.config.staticFeedsBaseUrl}/${subname}/${static_alert_file}".toString());
+      new_item_node.appendNode( 'link', "${grailsApplication.config.staticFeedsBaseUrl}/${subname}${static_alert_file}".toString());
       new_item_node.appendNode( 'description', info?.description);
-      new_item_node.appendNode( 'pubDate', node?.AlertBody?.sent);
+      new_item_node.appendNode( 'pubDate', formatted_pub_date ?: node?.AlertBody?.sent);
       //      //'dc:creator'('creator')
       //      //'dc:date'('date')
 
@@ -152,7 +144,7 @@ class StaticFeedService {
       // The true asks the sort to mutate the source list. Source elements without a pubDate element high - so the none item
       // entries float to the top of the list
       xml.channel[0].children().sort(true) { a,b ->
-        ( b.pubDate?.text() ?: 'aaa'+(b.name().toString() ) ).compareTo( ( a.pubDate?.text() ?: 'aaa'+(a.name().toString() ) ) )
+        ( b.'link'?.text() ?: 'zzz'+(b.name().toString() ) ).compareTo( ( a.'link'?.text() ?: 'zzz'+(a.name().toString() ) ) )
       }
 
       log.debug("Trim rss feed. Size before: ${xml.channel[0].children().size()}");
@@ -216,7 +208,7 @@ class StaticFeedService {
     def cal = Calendar.getInstance()
     cal.setTime(alert_date);
     // def alert_path = "${cal.get(Calendar.YEAR)}/${cal.get(Calendar.MONTH)}/${cal.get(Calendar.DAY_OF_MONTH)}/${cal.get(Calendar.HOUR_OF_DAY)}}/${cal.get(Calendar.MINUTE)}"
-    def alert_path = sprintf('/%02d/%02d/%02d/%02d/%02d/',[cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH),cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE)]);
+    def alert_path = sprintf('/%02d/%02d/%02d/%02d/%02d/',[cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1,cal.get(Calendar.DAY_OF_MONTH),cal.get(Calendar.HOUR_OF_DAY),cal.get(Calendar.MINUTE)]);
     log.debug("Write to ${path}${alert_path}")
 
     File alert_path_dir = new File(path+alert_path)
