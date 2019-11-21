8ad58f5f3b135dc66501c9ad3f9551005bcd2ce3
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-21 10:51:55 +0000

    Added build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+sdk use grails 4.0.1
+sdk use java 11.0.5.j9-adpt
+cd CAPAggregator
+grails prod war
+cp build/libs/CAPAggregator-2.0.0.war ../docker/CAPAggregator.war
+cd ../docker
+docker login
+docker build -t semweb/caphub_aggregator:v2.0 -t semweb/caphub_aggregator:v2 -t semweb/caphub_aggregator:latest .
+docker push semweb/caphub_aggregator:v2.0
+docker push semweb/caphub_aggregator:v2
+docker push semweb/caphub_aggregator:latest
+
+
