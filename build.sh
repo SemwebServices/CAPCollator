d59a542687585d283f6899902770cf685b132f17
Ian Ibbotson <ianibbo@gmail.com>
2019-12-24 11:13:12 +0000

    update build script

--- build.sh
+++ build.sh
@@ -5,14 +5,6 @@ export SDKMAN_DIR="/home/ibbo/.sdkman"
 export CC_VER=`grep appVersion ./CAPAggregator/gradle.properties | cut -f2 -d=`
 echo Releasing CAPAggregator ${CC_VER}
 
-if [[ "$CC_VER" == *-SNAPSHOT ]]
-then
-  echo  SNAPSHOT release
-else
-  echo  Standard Release
-fi
-
-
 sdk use grails 4.0.1
 sdk use java 11.0.5.j9-adpt
 cd CAPAggregator
@@ -20,10 +12,22 @@ grails prod war
 cp build/libs/CAPAggregator-*.war ../docker/CAPAggregator.war
 cd ../docker
 docker login
-docker build -t semweb/caphub_aggregator:v$CC_VER -t semweb/caphub_aggregator:v2.0 -t semweb/caphub_aggregator:v2 -t semweb/caphub_aggregator:latest .
-docker push semweb/caphub_aggregator:v$CC_VER
-docker push semweb/caphub_aggregator:v2.0
-docker push semweb/caphub_aggregator:v2
-docker push semweb/caphub_aggregator:latest
+
+if [[ "$CC_VER" == *-SNAPSHOT ]]
+then
+  echo  SNAPSHOT release
+  docker build -t semweb/caphub_aggregator:v$CC_VER -t semweb/caphub_aggregator:latest .
+  docker push semweb/caphub_aggregator:v$CC_VER
+  docker push semweb/caphub_aggregator:latest
+else
+  echo  Standard Release
+  docker build -t semweb/caphub_aggregator:v$CC_VER -t semweb/caphub_aggregator:v2.0 -t semweb/caphub_aggregator:v2 -t semweb/caphub_aggregator:latest .
+  docker push semweb/caphub_aggregator:v$CC_VER
+  docker push semweb/caphub_aggregator:v2.0
+  docker push semweb/caphub_aggregator:v2
+  docker push semweb/caphub_aggregator:latest
+fi
+
+
 
 
